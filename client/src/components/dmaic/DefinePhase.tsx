007d5a96949056cf3521487022c11ff5b3ac352f
franckstrub61 <42036271-franckstrub61@users.noreply.replit.com>
2025-05-05 11:53:29 +0000

    Restored to 'a76e9606463071b5b11e8629863783cf7fe69746'
    Replit-Restored-To: a76e9606463071b5b11e8629863783cf7fe69746

--- client/src/components/dmaic/DefinePhase.tsx
+++ client/src/components/dmaic/DefinePhase.tsx
@@ -16,7 +16,6 @@ import { Badge } from "@/components/ui/badge";
 import { exportElementToPdf } from "@/lib/pdfExport";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger, DialogFooter } from "@/components/ui/dialog";
 import MilestoneTimeline from "./MilestoneTimeline";
-import { ProgressCursor } from "./ProgressCursor";
 import {
   Card,
   CardContent,
